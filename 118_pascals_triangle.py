8d34f71c49978b3439bb410320be9a8e69d79f21
bakerv <80018640+bakerv@users.noreply.github.com>
2021-12-21 22:33:26 -0600

    Appending nested arrays to build each layer of values for pascals triangle.

---
+++ 118_pascals_triangle.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        for row in range(1, numRows + 1):
+            row_contents = []
+            value = 1
+            for element in range(1, row + 1):
+                row_contents.append(value)
+                value = value * (row - element) / element
+            result.append(row_contents)
+
+        return result
+
+# solution modified from geeks for geeks article on printing pascals triangle
