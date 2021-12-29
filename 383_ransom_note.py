1c1178073799fafbad4d6b7ed90c53919ab4eb0d
bakerv <80018640+bakerv@users.noreply.github.com>
2021-12-28 18:48:32 -0600

    Used the python collections library to implement a solution of O(n) time complexity.

---
+++ 383_ransom_note.py
@@ -0,0 +1,27 @@
+import collections
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+        note_count = collections.Counter(magazine)
+        for char in ransomNote:
+            # exit if the character is not in the magazine
+            if char not in note_count:
+                return False
+
+            # update availability for each character
+            note_count[char] += -1
+            if note_count[char] < 0:
+                return False
+
+        return True
+
+ransom_note='aa'
+magazine='ab'
+test = Solution()
+print(test.canConstruct(ransom_note, magazine))
