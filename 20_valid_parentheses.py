08265af433fc6aeec1e5ed8802bc7ef91a57662f
bakerv <80018640+bakerv@users.noreply.github.com>
2022-01-12 14:54:06 -0600

    updated comments

--- 20_valid_parentheses.py
+++ 20_valid_parentheses.py
@@ -3,8 +3,8 @@ import collections
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        # empty stack to monitor the required bracket sequence
         symbol_stack = collections.deque()
-        # for each open
         for symbol in s:
             # for each opening backet, add the required closing bracket to the stack
             if symbol == '(':
