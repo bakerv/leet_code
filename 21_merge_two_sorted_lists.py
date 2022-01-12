06ace9068806b362ceb099f0f7bd787c47b8b0cf
bakerv <80018640+bakerv@users.noreply.github.com>
2022-01-11 19:21:05 -0600

    Correctly solved the problem for linked lists, not arrays.

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -10,10 +10,25 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        return sorted(list(sum([list1, list2], [])))
+        # create a head for a new linked list
+        final_list_head = current_location = ListNode(0)
 
-list1 = [1,2,4,7, 5]
+        # add the location of the smaller value as the next element of the new list
+        # and advance the list that contained the smaller value
+        # as well as the new list
+        while list1 and list2:
+            if list1.val < list2.val:
+                current_location.next = list1
+                list1 = list1.next
+            else:
+                current_location.next = list2
+                list2 = list2.next
+            current_location = current_location.next
 
-list2 = [1,3,4]
-test = Solution()
-print(test.mergeTwoLists(list1, list2))
+        # after exhausting the shorter list
+        # aim the pointer at the remaining sorted list
+        current_location.next = list1 or list2
+
+        # return only elements from list1 and list2
+        # not the head from the new list
+        return final_list_head.next
